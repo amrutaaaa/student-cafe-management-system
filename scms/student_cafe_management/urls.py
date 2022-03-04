0311140b6c451d0e1d2e326037b7fc917caf01a5
Vidhi Mittal <you@example.com>
2022-03-04 20:11:42 +0530

    demo page created

---
+++ scms/student_cafe_management/urls.py
@@ -0,0 +1,4 @@
+from django.urls import path
+from . import views  # . means same directory
+
+urlpatterns=[path('', views.index, name='index'),]
--- scms/student_cafe_management/views.py
+++ scms/student_cafe_management/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
+
+
+def index(request):
+    return render(request, 'student_cafe_management/index.html')
+
 # Create your views here.
